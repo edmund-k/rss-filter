a1e19e467637275fc827d71cad64725d137ad069
edmund-k <github@edmundk.de>
2019-02-03 20:16:10 +0100

    Temporary debugging

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ header('Content-Type: text/xml; charset=utf-8');
 
 include 'rss-filter.php';
 $rf = new RssFilter();
+//echo $rf->init('https://ticker.mvg.de/');
 echo $rf->init($_GET['source']);
+echo "\n";
 ?>
